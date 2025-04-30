3796aee66fd8b87f77c30ac649b5968b17fbb37f
ri-izdo <me@riizdo.com>
2025-04-29 20:20:58 -0700

    Add readme.

--- SwiftlyRod/SwiftlyRodApp.swift
+++ SwiftlyRod/SwiftlyRodApp.swift
@@ -23,36 +23,3 @@ struct StravaApp: App {
         }
     }
 }
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    let strava: StravaClient
-
-    override init() {
-        let config = StravaConfig(
-            clientId: 150395,
-            clientSecret: "6c0c5ae5f08d52c6cbc68bd8aa0206963c64759c",
-            redirectUri: "swiftlyrod://rodericklizardo.com",
-            scopes: [.profileReadAll, .activityReadAll, .activityWrite] // Ensure profileReadAll is included
-        )
-        strava = StravaClient.sharedInstance.initWithConfig(config)
-
-        super.init()
-        print("✅ StravaClient initialized with scopes: \(config.scopes)")
-    }
-
-
-
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        print("🔄 Received Redirect: \(url.absoluteString)")
-
-        let handled = StravaClient.sharedInstance.handleAuthorizationRedirect(url)
-
-        if handled {
-            print("✅ Successfully handled Strava Redirect")
-        } else {
-            print("❌ Strava Redirect NOT handled")
-        }
-
-        return handled
-    }
-}
